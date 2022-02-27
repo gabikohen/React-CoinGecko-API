0b246a2b311b5464768e9f86d7d28182a60dca83
gabikohen <gabokohen@gmail.com>
2022-02-27 17:23:46 -0300

    terminado

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import "./App.css";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import TableCoin from "./components/TableCoin";
-import CoinRow from "./components/CoinRow";
+
+
 function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
   const getData = async () => {
-    const res = await axios.get(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-    );
-    console.log(res.data);
-    setCoins(res.data);
+    try {
+      const res = await axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      );
+      setCoins(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
@@ -18,10 +25,17 @@ function App() {
   }, []);
   return (
     <div className="cointainer">
-      <div className="row"></div>
-      
-      <TableCoin coins={coins} />
-      <CoinRow />
+      <div className="row">
+      <input
+          type="text"
+          placeholder="Search a Coin"
+          className="form-control bg-dark text-light border-0 mt-4 text-center"
+          autoFocus
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <TableCoin coins={coins}  search={search} />
+        
+      </div>
     </div>
   );
 }
--- src/components/TableCoin.js
+++ src/components/TableCoin.js
@@ -1,31 +1,28 @@
-import React from 'react'
-import CoinRow from './CoinRow'
+import React from "react";
+import CoinRow from "./CoinRow";
 
+const titles = ["#", "Coin", "Price", "Price Change","24h Volume"];;
 
-const titulo = ['#','Price','Price Change','24h Volume']
+function TableCoin({ coins,search}) {
 
-function TableCoin({coins}) {
-    
+  const filteredCoins = coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()) || coin.symsol.toLowerCase().includes(search.toLowerCase()) );
   return (
-    <table className='table table-dark mt-4 table-hover'>
-        <thead>
-            <tr>
-              {
-               titulo.map(title => (
-                  <td>{title}</td>
-                ))
-              }
-               <td>name</td>
-            </tr>
-            </thead>
-            <tbody>
-                  {coins.map((coin,index) => 
-                     <CoinRow coin={coin} key={index} index={index  + 1} />
-                  )}
-            </tbody>
-       
+    <table className=" table table-dark mt-4  table-hover">
+      <thead>
+        <tr>
+          {titles.map((title, i) => (
+            <td key={i}>{title}</td>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {filteredCoins.map((coin, index) => (
+          <CoinRow key={coin.id} coin={coin} index={index + 1} />
+        ))}
+      </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default TableCoin
+
+export default TableCoin;
